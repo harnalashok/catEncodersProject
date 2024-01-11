64e6da70396cf4b533aa6bfee9ee4360dfea52ae
Anjanay <77952502+anjanay-anjanay@users.noreply.github.com>
2024-01-11 23:44:53 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,16 +3,11 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-
 import os
 import sys
 
 # Add the directory containing the Python file to sys.path
 sys.path.insert(0, os.path.abspath('/home/ashok/hosting_sample'))
-
 
 project = 'Sample'
 copyright = '2024, Anjanay'
@@ -25,11 +20,11 @@ release = '1.0.1'
 extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'abcd.rst', 'xyz.rst']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
 html_theme = 'sphinx_rtd_theme'
 
 extensions = [
@@ -45,4 +40,13 @@ autodoc_default_options = {
     'private-members': True,
     'special-members': True,
     'show-inheritance': True,
+}
+
+# Link to the GitHub repository
+html_context = {
+    'display_github': True,
+    'github_user': 'your_github_user',
+    'github_repo': 'your_repo_name',
+    'github_version': 'main/',
+    'github_url': 'https://github.com/harnalashok/catEncodersProject',
 }
